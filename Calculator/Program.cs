53abb81442972bd4d233f142b9c8135e0447bf01
Samuel Araujo <samuel.araujo.carvalho@gmail.com>
2024-03-17 17:31:19 -0300

    Calculator First

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(v1);
+        }
+    }
+}
